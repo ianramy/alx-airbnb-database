c90ce79720604f5fb6250f889e7bc55c26b68fdc
Ian Ramy <imwagore@gmail.com>
2025-05-17 10:00:13 +0300

    Updated database_index.sql

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -5,3 +5,14 @@ CREATE INDEX idx_property_id ON bookings(property_id);
 CREATE INDEX idx_start_date ON bookings(start_date);
 CREATE INDEX idx_reviews_property_id ON reviews(property_id);
 CREATE INDEX idx_users_id ON users(id);
+
+-- AFTER indexing:
+EXPLAIN SELECT * FROM bookings WHERE user_id = 42;
+
+-- AFTER indexing:
+EXPLAIN SELECT u.name, b.start_date
+FROM bookings b
+JOIN users u ON b.user_id = u.id;
+
+-- AFTER indexing:
+EXPLAIN SELECT * FROM bookings WHERE start_date BETWEEN '2025-01-01' AND '2025-12-31';
